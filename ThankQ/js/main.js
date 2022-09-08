1e313f46473d8aa2efa3f154ea7f17edd442fa7c
Hoseon <96182335+Hoseons@users.noreply.github.com>
2022-09-08 22:39:58 +0900

    22.09.08

--- ThankQ/js/main.js
+++ ThankQ/js/main.js
@@ -3,4 +3,18 @@ $(document).ready(function(){
     $(this).children('.sub').slideDown()
   },function(){
     $(this).children('.sub').slideUp()})
+  
+    var swiper1 = new Swiper('.swiper1', {
+      slidesPerView: 1.5,
+      freeMode: true,
+      spaceBetween: 10,
+    });
+    
+    var swiper2 = new Swiper('.swiper2', {
+      slidesPerView: 1.8,
+      freeMode: true,
+      spaceBetween: 10,
+      
+    });
+  
 })
